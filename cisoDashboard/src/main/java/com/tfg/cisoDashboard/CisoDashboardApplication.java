28f5b4d4cbec672229c8daf88d0e74a6b63f7dd6
Mohsin Zaman <mohsinstyle786@gmail.com>
2024-05-11 22:36:08 +0200

    Addin Spring application fro backend

---
+++ cisoDashboard/src/main/java/com/tfg/cisoDashboard/CisoDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.cisoDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CisoDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CisoDashboardApplication.class, args);
+	}
+
+}
