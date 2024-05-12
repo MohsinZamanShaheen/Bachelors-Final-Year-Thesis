5c61c54d56d338ce0d0fbaea1f920f54bba41219
Mohsin Zaman <mohsinstyle786@gmail.com>
2024-05-12 23:24:19 +0200

    Some touchings to Network Diagram. Added header also

--- cisoDashboard/frontend/src/Pages/inventory/networkdiagram/network_diagram.jsx
+++ cisoDashboard/frontend/src/Pages/inventory/networkdiagram/network_diagram.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback } from "react";
 import ReactFlow, {
   addEdge,
   MiniMap,
@@ -6,24 +6,25 @@ import ReactFlow, {
   Background,
   useNodesState,
   useEdgesState,
-} from 'reactflow';
+} from "reactflow";
+import { Box } from "@mui/material";
+import Header from "../../../Components/global/Header";
 
 import {
   nodes as initialNodes,
   edges as initialEdges,
-} from './initial_elements';
-import TransparentNode from './TransparentNode';
-import 'reactflow/dist/style.css';
-import './overview.css';
-import GroupNode from './GroupNode';
+} from "./initial_elements";
+import TransparentNode from "./TransparentNode";
+import "reactflow/dist/style.css";
+import "./overview.css";
+import GroupNode from "./GroupNode";
 
 const nodeTypes = {
   transparent: TransparentNode,
-  group: GroupNode
+  group: GroupNode,
 };
 
-const edgeTypes = {
-};
+const edgeTypes = {};
 
 const nodeClassName = (node) => node.type;
 
@@ -32,26 +33,34 @@ const OverviewFlow = () => {
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
   const onConnect = useCallback(
     (params) => setEdges((eds) => addEdge(params, eds)),
-    [],
+    []
   );
 
   return (
-    <ReactFlow
-      nodes={nodes}
-      edges={edges}
-      onNodesChange={onNodesChange}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
-      fitView
-      attributionPosition="top-right"
-      nodeTypes={nodeTypes}
-      edgeTypes={edgeTypes}
-      className="overview"
-    >
-      <MiniMap zoomable pannable nodeClassName={nodeClassName} />
-      <Controls />
-      <Background />
-    </ReactFlow>
+    <Box sx={{width:"100%", height:"100%"}}>
+      <Box m="20px">
+        <Header
+          title="Network Diagram"
+          subtitle="Summary of the organization's Network Diagram and connections between devices"
+        />
+      </Box>
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          fitView
+          attributionPosition="top-right"
+          nodeTypes={nodeTypes}
+          edgeTypes={edgeTypes}
+          className="overview"
+        >
+          <MiniMap zoomable pannable nodeClassName={nodeClassName} />
+          <Controls />
+          <Background />
+        </ReactFlow>
+    </Box>
   );
 };
 
